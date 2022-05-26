9d0d555c807d25c14347e4fdd3f86f2f9c8c6339
Ssdklf2 <hsnev@bk.ru>
2022-05-26 19:20:19 +0300

    Changes 2

--- src/w1d4/Reader.java
+++ src/w1d4/Reader.java
@@ -8,22 +8,24 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Reader  implements Callable<ConcurrentLinkedQueue<String>> {
+public class Reader implements Callable<ConcurrentLinkedQueue<String>> {
+
     public static final String VOYNA_I_MIR_TXT = "src\\w1d4\\voyna-i-mir.txt";
-    public String[] array = {"ружье","здоровье", "туман", "кабинет", "год", "месяц", "лошадь"};
+    public String[] array = {"ружье", "здоровье", "туман", "кабинет", "год", "месяц", "лошадь"};
     String paragraph;
     ConcurrentLinkedQueue<String> stringConcurrentLinkedQueue = new ConcurrentLinkedQueue<>();
 
     /**
      * Читает из файла абзацы, и ищет в них подходящие предложения
+     *
      * @param array    - массив слов, которые надо искать
      * @param fileName - имя файла
      * @return предложения, в которых есть слова из массива
      */
     public ConcurrentLinkedQueue<String> reade(String[] array, String fileName) {
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-            while ((paragraph = br.readLine())!=null) {
-                makeSentence(array, paragraph, stringConcurrentLinkedQueue);
+            while ((paragraph = br.readLine()) != null) {
+                extractSentence(array, paragraph, stringConcurrentLinkedQueue);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -32,28 +34,32 @@ public class Reader  implements Callable<ConcurrentLinkedQueue<String>> {
     }
 
     /**
-     * Создает предложение
-     * @param array - массив слов, которые надо искать
+     * Находит предложения из текста
+     *
+     * @param array     - массив слов, которые надо искать
      * @param paragraph - абзац
-     * @param strings - предложение, в котором ищется слова из массива
+     * @param strings   - предложение, в котором ищется слова из массива
      */
-    private void makeSentence(String[] array, String paragraph, ConcurrentLinkedQueue<String> strings) {
+    private void extractSentence(String[] array, String paragraph,
+                                 ConcurrentLinkedQueue<String> strings) {
         String sentence;
         Pattern pattern = Pattern.compile("[^.!?\\[]+[.!?]");
         Matcher matcher = pattern.matcher(paragraph);
         while (matcher.find()) {
-            sentence= matcher.group();
+            sentence = matcher.group();
             searchSentences(array, sentence, strings);
         }
     }
 
     /**
      * Ищет слова из массива в предложениях
-     * @param array - массив слов, которые надо искать
+     *
+     * @param array    - массив слов, которые надо искать
      * @param sentence - предложения
-     * @param strings - добавляются подходящие предложения
+     * @param strings  - добавляются подходящие предложения
      */
-    private void searchSentences(String[] array, String sentence, ConcurrentLinkedQueue<String> strings) {
+    private void searchSentences(String[] array, String sentence,
+                                 ConcurrentLinkedQueue<String> strings) {
         for (String word : array) {
             if (sentence.contains(" " + word + " ")) {
                 strings.add(sentence);
--- src/w1d4/Start.java
+++ src/w1d4/Start.java
@@ -7,8 +7,8 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class Start {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         ExecutorService executorReader = Executors.newFixedThreadPool(10);
         Future<ConcurrentLinkedQueue<String>> submit = executorReader.submit(new Reader());
         executorReader.shutdown();
