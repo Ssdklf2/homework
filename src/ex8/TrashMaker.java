e9e6a2dd2f45887b8869108a46f6330ee66c43fd
Ssdklf2 <hsnev@bk.ru>
2022-07-04 17:39:56 +0300

    ex8

---
+++ src/ex8/TrashMaker.java
@@ -0,0 +1,36 @@
+package ex8;
+
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+
+/**
+ * Необходимо создать программу, которая продемонстрирует утечку памяти в Java.
+ * При этом объекты должны не только создаваться, но и периодически частично удаляться,
+ * чтобы GC имел возможность очищать часть памяти.
+ * Через некоторое время программа должна завершиться с ошибкой OutOfMemoryError c пометкой Java Heap Space.
+ * <p>
+ * Задание со звездочкой: сделать чтобы ошибка OutOfMemoryError была в Permanent Generation (или Metaspace)
+ */
+public class TrashMaker {
+
+    public static void main(String[] args) throws IOException {
+        new TrashMaker().doGarbage();
+    }
+
+    public void doGarbage() throws IOException {
+        ArrayList<String> strings = new ArrayList<>();
+        ArrayList<ArrayList<String>> listOfStrings = new ArrayList<>();
+        OutputStreamWriter writer = new OutputStreamWriter(new ByteOutputStream());
+        for (int i = 0; i < 1_000_000; i++) {
+            String trash = String.valueOf(Math.random() * 10000);
+            strings.add(trash.intern());
+        }
+        for (int i = 0; i < 1_000; i++) {
+            listOfStrings.add(strings);
+        }
+        writer.write(String.valueOf(listOfStrings));
+    }
+}
